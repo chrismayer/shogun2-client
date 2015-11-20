3f98b9d0e8d774d4dda33da2249bf7087a40d7c0
Daniel Koch <koch@terrestris.de>
2015-11-20 09:41:13 +0100

    Fix and add MapController basic tests

--- test/spec/view/component/MapController.test.js
+++ test/spec/view/component/MapController.test.js
@@ -24,12 +24,90 @@ describe('ShogunClient.view.component.MapController', function() {
     describe('Methods', function() {
         describe('#setMap', function() {
             it('is defined', function() {
+                expect(mapController.setMap).to.not.be(undefined);
+            });
+            it('is a function', function() {
                 expect(mapController.setMap).to.be.a('function');
             });
         });
-        describe('#setControls', function() {
+        describe('#createOlMapControls', function() {
             it('is defined', function() {
-                expect(mapController.setControls).to.be.a('function');
+                expect(mapController.createOlMapControls).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlMapControls).to.be.a('function');
+            });
+        });
+        describe('#createOlMap', function() {
+            it('is defined', function() {
+                expect(mapController.createOlMap).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlMap).to.be.a('function');
+            });
+        });
+        describe('#createOlMapRenderer', function() {
+            it('is defined', function() {
+                expect(mapController.createOlMapRenderer).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlMapRenderer).to.be.a('function');
+            });
+        });
+        describe('#createOlMapView', function() {
+            it('is defined', function() {
+                expect(mapController.createOlMapView).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlMapView).to.be.a('function');
+            });
+        });
+        describe('#createOlLayers', function() {
+            it('is defined', function() {
+                expect(mapController.createOlLayers).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlLayers).to.be.a('function');
+            });
+        });
+        describe('#createOlLayer', function() {
+            it('is defined', function() {
+                expect(mapController.createOlLayer).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlLayer).to.be.a('function');
+            });
+        });
+        describe('#createOlLayerSource', function() {
+            it('is defined', function() {
+                expect(mapController.createOlLayerSource).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlLayerSource).to.be.a('function');
+            });
+        });
+        describe('#createOlLayerTileGrid', function() {
+            it('is defined', function() {
+                expect(mapController.createOlLayerTileGrid).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlLayerTileGrid).to.be.a('function');
+            });
+        });
+        describe('#createOlLayerAttribution', function() {
+            it('is defined', function() {
+                expect(mapController.createOlLayerAttribution).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.createOlLayerAttribution).to.be.a('function');
+            });
+        });
+        describe('#getProjectionString', function() {
+            it('is defined', function() {
+                expect(mapController.getProjectionString).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(mapController.getProjectionString).to.be.a('function');
             });
         });
     });
